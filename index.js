3a7eed1067580ac038b3c27c5445b01d7b746774
Patrick Huynh <pldhuynh@gmail.com>
2020-06-24 19:28:22 +1000

    test defintiion create function

--- index.js
+++ index.js
@@ -7,21 +7,48 @@ const router = createRouter();
 
 module.context.use(router);
 
+// general object insert function
+const insertDocument = (collection, document) => {
+  const coln = db._collection(collection);
+  const meta = coln.save(data);
+  return meta;
+};
+
 // objects CRUD
+// create object
 router
   .post("/insert/:collection/", (req, res) => {
-    const data = req.body;
-    const colnName = req.pathParams.collection;
-    const coln = db._collection(colnName);
-    const meta = coln.save(data);
-    res.send(Object.assign(data, meta));
+    const document = req.body;
+    const collectionName = req.pathParams.collection.toLowerCase();
+    const meta = insertDocument(collectionName, document);
+    res.send(Object.assign(document, meta));
   })
   .body(joi.object().required(), "Document to store in the collection")
-  .response(joi.object().required(), "Document to stored in the collection");
-
-// create object
+  .pathParam("collection", joi.string().required(), "Collection name")
+  .response(joi.object().required(), "Document to stored in the collection")
+  .summary("Creates a document")
+  .description("Creates a document in the collection by key.");
 
 // get object
+router
+  .post("/get/:collection/:key", (req, res) => {
+    try {
+      const collectionName = req.pathParams.collection.toLowerCase();
+      const key = req.pathParams.key.toLowerCase().replace(" ", "_");
+      const data = db._collection(collectionName).document(key);
+      res.send(data);
+    } catch (e) {
+      if (!e.isArangoError || e.errorNum !== DOC_NOT_FOUND) {
+        throw e;
+      }
+      res.throw(404, "The entry does not exist", e);
+    }
+  })
+  .pathParam("collection", joi.string().required(), "Collection name")
+  .pathParam("key".object().required(), "Key of document in collection")
+  .response(joi.object().required(), "Document to stored in the collection")
+  .summary("Retrieve an entry")
+  .description("Retrieves an entry from the collection by key.");
 
 // update object
 
